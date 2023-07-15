5ae6a2c5790273fd4a2315a7c1f37d2edd7e4365
mavene <dianmaisara@gmail.com>
2023-07-15 19:31:54 +0800

    fix type-ing in Dashboard code

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,7 +4,66 @@ import addServiceProviderIcon from '../images/add_service_provider_icon.svg';
 import filterIcon from '../images/filter_icon.svg';
 import { useNavigate, useLocation } from "react-router-dom";
 
-// making a dashbaord component
+// old Dashboard for demo
+// function Dashboard() {
+//   const navigate = useNavigate();
+//   const locate = useLocation();
+
+//   // // TODO: Opt-in option for users (tenant, landlords)
+//   // //  Mock static values
+//   // var ticket_id = "007";
+//   // var ticket_title = "Mock Ticket Title";
+//   // var location = "Sunplaza";
+//   // var unit = "01-35";
+
+//   const formState = locate.state? locate.state.formState : null;
+//   var isClosed = locate.state? locate.state.isClosed : null;
+//   var ticket_id = locate.state ? "007" : "TID";
+//   var ticket_title = locate.state? formState.formTitle : "Ticket Title";
+//   var submitStatus = locate.state? locate.state.isSubmit : false;
+//   var building = "SunPlaza";
+//   var unit = "01-42";
+
+//   if (locate.state) {
+//       console.log(locate.state);
+
+// return (
+//   <div className="flex flex-col font-3xl" id="viewTicket">
+//         <div className='flex justify-center'>
+//             <p className='text-headerText py-5 text-2xl font-medium'>Dashboard (TODO)</p>
+//         </div>
+//         <div className="flex flex-row self-center">
+//         <div className="flex w-fit bg-form border-gray-200 rounded-lg shadow sm:p-7">
+//             <form className="space-y-4">
+//                 <p className="text-lg text-left font-medium">#{ticket_id} : {ticket_title} ({building} #{unit}) </p>
+//                 <hr className="h-[1px] bg-gray-300 border-0 drop-shadow-md"></hr>
+//                 {submitStatus ? 
+//                     <ActionButton
+//                     value={"View Details"}
+//                     padding_right={""}
+//                     type=""
+//                     firstViewState={false}
+//                     toggle={false}
+//                     onClick={()=>navigate('/viewDetails', {state: {formState, isSubmit: false, isClosed }})} />
+//                 : ""}
+//             </form>
+//         </div>
+//         <div className="flex">
+//             <ActionButton
+//                     value={"New Request"}
+//                     padding_right={"20"}
+//                     type=""
+//                     firstViewState={false}
+//                     toggle={false}
+//                     onClick={()=>navigate('/newRequest')} />
+//         </div>
+//         </div>
+//   </div>
+// );
+// }
+
+// export default Dashboard;
+
 const Dashboard = () => {
   // useStates
   const [userIsActive, setUserIsActive] = useState(false);
@@ -43,7 +102,7 @@ const Dashboard = () => {
   };
 
   // Check all checkbox function using indeterminate checkbox
-  const [checked, setChecked] = useState([]);
+  const [checked, setChecked] = useState<any>([]);
 
   const handleCheckAll = () => {
     if (checked.length === tableData.length) {
@@ -54,11 +113,11 @@ const Dashboard = () => {
     }
   };
 
-  const handleCheck = (itemId) => {
+  const handleCheck = (itemId: number) => {
     if (checked.includes(itemId)) {
-      setChecked((prevItems) => prevItems.filter((id) => id !== itemId));
+      setChecked((prevItems: any[]) => prevItems.filter((id: any) => id !== itemId));
     } else {
-      setChecked((prevItems) => [...prevItems, itemId]);
+      setChecked((prevItems: any) => [...prevItems, itemId]);
     }
   };
 
@@ -72,7 +131,7 @@ const Dashboard = () => {
   ];
 
   // Function to update the status of the row for dropdown selection
-  const handleStatusUpdate = (itemId, e) => {
+  const handleStatusUpdate = (itemId: number, e: React.ChangeEvent<HTMLSelectElement>) => {
     const updateTableData = tableData.map((row) => {
       if (row.ID === itemId) {
         return { ...row, Status: e.target.value };
@@ -83,7 +142,7 @@ const Dashboard = () => {
   };
 
   // Function for delete row
-  const deleteRow = (rowId) => {
+  const deleteRow = (rowId: number[]) => {
     let copy = [...tableData];
     copy = copy.filter((row) => !rowId.includes(row.ID));
     setTableData(copy);
