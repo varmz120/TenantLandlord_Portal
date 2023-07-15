6eed2479b0dc69bf03cef683055d61aae25d50c8
Jone <jone.chong99@gmail.com>
2023-07-15 13:46:03 +0800

    Finished admin homepage except for integration and responsive design

--- src/pages/AdminHome.tsx
+++ src/pages/AdminHome.tsx
@@ -46,45 +46,81 @@ const AdminHome = () => {
     setdatabaseIsActive(false);
   };
 
+  const buildingsOnClick = () => {
+    console.log('buildings clicked');
+  };
+
+  const accountsOnClick = () => {
+    console.log('accounts clicked');
+  };
+
+  const webAppOnClick = () => {
+    console.log('webapp clicked');
+  };
+
+  const databaseOnClick = () => {
+    console.log('database clicked');
+  };
+
   return (
-    <div className="AdminHome bg-[#ECEDED] flex-1">
-      <div className="flex items-center justify-center">
-        <p className="text-xl">Administrator Panel</p>
-        <img className="ml-2" src={gearIcon} alt="Settings Icon" />
+    <div className="AdminHome bg-[#ECEDED] flex-1 font-bold w-full">
+      <div className="flex items-center justify-center mt-64 mb-16 sm:">
+        <p className="text-3xl">Administrator Panel</p>
+        <img
+          className="ml-4 w-auto h-auto max-w-full max-h-full"
+          src={gearIcon}
+          alt="Settings Icon"
+        />
       </div>
       <div className="flex items-center justify-center">
         <div className="flex">
           <div
-            className="flex flex-col w-32 h-32 bg-white hover:bg-[#31556F] cursor-pointer mr-2 items-center justify-center"
+            className="flex flex-col w-40 h-40 bg-white hover:bg-[#31556F] cursor-pointer mr-2 items-center justify-center rounded-xl"
             onMouseEnter={handleBuildingsActive}
             onMouseLeave={handleBuildingsInactive}
+            onClick={buildingsOnClick}
           >
             <p className={buildingsIsActive ? 'text-white' : 'text-black'}>Buildings</p>
-            <img src={buildingsIsActive ? buildingsIconDark : buildingsIcon} />
+            <img
+              src={buildingsIsActive ? buildingsIconDark : buildingsIcon}
+              className="mt-3 w-auto h-auto max-w-full max-h-full"
+            />
           </div>
           <div
-            className="flex flex-col  w-32 h-32 bg-white hover:bg-[#31556F] cursor-pointer mr-16 items-center justify-center"
+            className="flex flex-col w-40 h-40 bg-white hover:bg-[#31556F] cursor-pointer mr-16 items-center justify-center rounded-xl"
             onMouseEnter={handleAccountsActive}
             onMouseLeave={handleAccountsInactive}
+            onClick={accountsOnClick}
           >
             <p className={accountsIsActive ? 'text-white' : 'text-black'}>Accounts</p>
-            <img src={accountsIsActive ? accountsIconDark : accountsIcon} className="mr-0" />
+            <img
+              src={accountsIsActive ? accountsIconDark : accountsIcon}
+              className="mt-3 w-auto h-auto max-w-full max-h-full"
+            />
           </div>
           <div
-            className="flex flex-col  w-32 h-32 bg-white hover:bg-[#31556F] cursor-pointer mr-2 items-center justify-center"
+            className="flex flex-col w-40 h-40 bg-white hover:bg-[#31556F] cursor-pointer mr-2 items-center justify-center rounded-xl"
             onMouseEnter={handleWebAppActive}
             onMouseLeave={handleWebAppInactive}
+            onClick={webAppOnClick}
           >
             <p className={webAppIsActive ? 'text-white' : 'text-black'}>Web App</p>
-            <img src={webAppIsActive ? webAppIconDark : webAppIcon} className="mr-0" />
+            <img
+              src={webAppIsActive ? webAppIconDark : webAppIcon}
+              className="mt-3 w-auto h-auto max-w-full max-h-full"
+            />
           </div>
           <div
-            className="flex flex-col w-32 h-32 bg-white hover:bg-[#31556F] cursor-pointer items-center justify-center"
+            className="flex flex-col w-40 h-40 bg-white hover:bg-[#31556F] cursor-pointer items-center justify-center rounded-xl"
             onMouseEnter={handleDatabaseActive}
             onMouseLeave={handleDatabaseInactive}
+            onClick={databaseOnClick}
           >
             <p className={databaseIsActive ? 'text-white' : 'text-black'}>Database</p>
-            <img src={databaseIsActive ? databaseIconDark : databaseIcon} className="mr-0" />
+            <img
+              src={databaseIsActive ? databaseIconDark : databaseIcon}
+              className="mt-3 w-auto h-auto max-w-full max-h-full"
+            />
           </div>
         </div>
       </div>
