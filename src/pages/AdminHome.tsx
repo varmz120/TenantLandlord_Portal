d8af234dbdc1afba02ac25b89064fab0dbd11e1b
Jone <jone.chong99@gmail.com>
2023-07-15 11:44:56 +0800

    Created AdminHome component/page

---
+++ src/pages/AdminHome.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdminHome = () => {
+  return (
+    <div>
+      <h1>Administrator Panel</h1>
+      <img src="images/gear_icon.svg" alt="Settings Icon" />
+    </div>
+  );
+};
+
+export default AdminHome;
