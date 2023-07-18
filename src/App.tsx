abd6aad63c49aff1acca910e297ea2c197cc5cfb
Darren Chan Yu Hao <darrenchanyuhao@gmail.com>
2023-07-19 00:43:01 +0800

    Change to Camelcase

--- src/App.tsx
+++ src/App.tsx
@@ -9,32 +9,29 @@ import RateTicket from './pages/RateTicket.tsx';
 import Landing from './pages/Landing.tsx';
 
 // Error Pages
-import ErrorPage404 from './pages/404.tsx';
-import ErrorPage401 from './pages/401.tsx';
-import ErrorPage403 from './pages/403.tsx';
-import RunTimeErrorPage from './pages/RuntimeError.tsx';
+import ErrorPage_404 from './pages/404.tsx';
+import ErrorPage_401 from './pages/401.tsx';
+import ErrorPage_403 from './pages/403.tsx';
+import ErrorPage_RunTime from './pages/RuntimeError.tsx';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthContextProvider } from './contexts/AuthContext.tsx';
 
 function App() {
   return (
-    <AuthContextProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/tenantDashboard" element={<DashboardPage />} />
-          <Route path="/newRequest" element={<RequestTicketPage />} />
-          <Route path="/viewDetails" element={<ViewTicketPage />} />
-          <Route path="/viewQuote" element={<ViewQuotePage />} />
-          <Route path="/feedbackSurvey" element={<RateTicketPage />} />
-          <Route path="/*" element={<ErrorPage404 />} />
-          <Route path="/401" element={<ErrorPage401 />} /> {/* To add proper auth routing */}
-          <Route path="/403" element={<ErrorPage403 />} /> {/* To add proper auth routing */}
-          <Route path="/Error" element={<RunTimeErrorPage />} /> {/* To add proper routing.*/}
-        </Routes>
-      </Router>
-    </AuthContextProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/newRequest" element={<RequestTicketPage />} />
+        <Route path="/viewDetails" element={<ViewTicketPage />} />
+        <Route path="/viewQuote" element={<ViewQuotePage />} />
+        <Route path="/feedbackSurvey" element={<RateTicketPage />} />
+        <Route path="/*" element={<ErrorPage_404 />} />
+        <Route path="/401" element={<ErrorPage_401 />} /> {/* To add proper auth routing */}
+        <Route path="/403" element={<ErrorPage_403 />} /> {/* To add proper auth routing */}
+        <Route path="/Error" element={<ErrorPage_RunTime />} /> {/* To add proper routing.*/}
+      </Routes>
+    </Router>
   );
 }
 
