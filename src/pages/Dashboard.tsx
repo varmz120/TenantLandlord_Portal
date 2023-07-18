fd68409e860a19700f88715649be193ebf1a536d
jonechong <jone.chong99@gmail.com>
2023-07-18 14:06:38 +0800

    Fixed dropdown for add service provider button hidden behind table

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -49,13 +49,13 @@ const Dashboard = () => {
     Status: '',
   });
 
-  // Implement Filter function for table 
+  // Implement Filter function for table
   const [filteredTableData, setFilteredTableData] = useState(tableData);
 
   const handleSearchInputChange = (column: TableColumn, value: string) => {
     setSearchInputs((prevState) => ({
       ...prevState,
-      [column]: value
+      [column]: value,
     }));
 
     const filteredData = tableData.filter((row) => {
@@ -130,7 +130,7 @@ const Dashboard = () => {
         return { ...row, Status: e.target.value };
       }
       return row;
-    })
+    });
     setFilteredTableData(updateFilteredTableData);
   };
 
@@ -151,204 +151,203 @@ const Dashboard = () => {
   const handleCategorySelect = (category: string) => {
     const updatedTableData = tableData.map((row) => {
       if (checked.includes(row.ID)) {
-        return {...row, Category: category};
+        return { ...row, Category: category };
       }
       return row;
     });
     setTableData(updatedTableData);
     setFilteredTableData(updatedTableData);
-  }
+  };
 
   const categoryOptions = [
-    {value: '', label: 'Selected Category'},
-    {value: 'Pest Exterminators', label: 'Pest Exterminators'},
-    {value: 'Plumbing', label: 'Plumbing'},
-    {value: 'Electrical', label: 'Electrical'},
-    {value: 'General Maintenance', label: 'General Maintenance'},
-  ]
+    { value: '', label: 'Selected Category' },
+    { value: 'Pest Exterminators', label: 'Pest Exterminators' },
+    { value: 'Plumbing', label: 'Plumbing' },
+    { value: 'Electrical', label: 'Electrical' },
+    { value: 'General Maintenance', label: 'General Maintenance' },
+  ];
 
   return (
     // Card component that will be used to display the data
     <div className="flex flex-col h-screen">
       <Navbar />
-      <div className='flex-grow flex flex-col h-screen bg-[#ECEDED] justify-center items-center'>
-        <div className="container mx-auto" style={{ maxWidth: '1329px', height: '656px'}}>
+      <div className="flex-grow flex flex-col h-screen bg-[#ECEDED] justify-center items-center">
+        <div className="container mx-auto" style={{ maxWidth: '1329px', height: '656px' }}>
           <div
-            className="flex items-center justify-between bg-[#31556F] rounded-t-lg drop-shadow-2xl"
-            style={{ height: '87px', paddingLeft: '20px', paddingRight: '20px' }}>
-          
+            className="flex items-center justify-between bg-[#31556F] rounded-t-lg drop-shadow-2xl z-10 relative"
+            style={{ height: '87px', paddingLeft: '20px', paddingRight: '20px' }}
+          >
             <div className="flex items-center">
               <button /* Clear Filter Button */
                 onClick={handleClearFilters}
                 className="block rounded flex border-solid border-1 px-2 py-1 mr-4
-                                            flex justify-center items-center text-[#3180ba] bg-[#edfdff] active:text-[#cbe6ec] active:bg-[#193446]">
+                                            flex justify-center items-center text-[#3180ba] bg-[#edfdff] active:text-[#cbe6ec] active:bg-[#193446]"
+              >
                 Clear Filters
               </button>
             </div>
             <div className="flex items-center">
               <a /* Filter Icon Button */
-                  href="#"
-                  className="block rounded-full px-5 py-5 mr-4
+                href="#"
+                className="block rounded-full px-5 py-5 mr-4
                                         flex items-center bg-[#edfdff] active:text-[#cbe6ec] active:bg-[#193446] "
-                  onMouseDown={handleUserActive}
-                  onMouseUp={handleUserInactive}
-                  onMouseLeave={handleUserInactive}
-                  onClick={toggleRowVisibility}
-                  style={{ width: '57px', height: '57px' }}
-                >
-                  <img
-                    src={userIsActive ? filterIcon : filterIcon}
-                    className="mx-auto scale-150"
-                    alt="?"
-                  ></img>
-                </a>
-                <a /* Delete Button */
+                onMouseDown={handleUserActive}
+                onMouseUp={handleUserInactive}
+                onMouseLeave={handleUserInactive}
+                onClick={toggleRowVisibility}
+                style={{ width: '57px', height: '57px' }}
+              >
+                <img
+                  src={userIsActive ? filterIcon : filterIcon}
+                  className="mx-auto scale-150"
+                  alt="?"
+                ></img>
+              </a>
+              <a /* Delete Button */
+                href="#"
+                className="block rounded-full px-5 py-5 mr-4
+                                        flex items-center bg-[#edfdff] active:text-[#cbe6ec] active:bg-[#193446]"
+                onMouseDown={handleUserActive}
+                onMouseUp={handleUserInactive}
+                onMouseLeave={handleUserInactive}
+                onClick={() => {
+                  deleteRow(checked);
+                  console.log(checked);
+                }}
+                style={{ width: '57px', height: '57px' }}
+              >
+                <img
+                  src={userIsActive ? trashBinIcon : trashBinIcon}
+                  className="mx-auto scale-150"
+                  alt="?"
+                ></img>
+              </a>
+              <div className="relative">
+                <a
                   href="#"
                   className="block rounded-full px-5 py-5 mr-4
-                                        flex items-center bg-[#edfdff] active:text-[#cbe6ec] active:bg-[#193446]"
+                      flex items-center bg-[#edfdff] active:text-[#cbe6ec] active:bg-[#193446]"
                   onMouseDown={handleUserActive}
                   onMouseUp={handleUserInactive}
                   onMouseLeave={handleUserInactive}
-                  onClick={() => {
-                    deleteRow(checked);
-                    console.log(checked);
-                  }}
+                  onClick={handleDropdownToggle}
                   style={{ width: '57px', height: '57px' }}
                 >
                   <img
-                    src={userIsActive ? trashBinIcon : trashBinIcon}
+                    src={userIsActive ? addServiceProviderIcon : addServiceProviderIcon}
                     className="mx-auto scale-150"
                     alt="?"
-                  ></img>
+                  />
                 </a>
-                <div className="relative">
-                  <a
-                    href="#"
-                    className="block rounded-full px-5 py-5 mr-4
-                      flex items-center bg-[#edfdff] active:text-[#cbe6ec] active:bg-[#193446]"
-                    onMouseDown={handleUserActive}
-                    onMouseUp={handleUserInactive}
-                    onMouseLeave={handleUserInactive}
-                    onClick={handleDropdownToggle}
-                    style={{ width: '57px', height: '57px' }}
-                  >
-                    <img
-                      src={userIsActive ? addServiceProviderIcon : addServiceProviderIcon}
-                      className="mx-auto scale-150"
-                      alt="?"
-                    />
-                  </a>
-                  {isDropdownOpen && (
-                    <div className="absolute right-0 mt-2 py-2 bg-white rounded shadow-lg">
-                      {categoryOptions.map((option) => (
-                        <button
-                          key={option.value}
-                          className="block px-4 py-2 text-gray-800 hover:bg-gray-100 w-full text-left"
-                          onClick={() => handleCategorySelect(option.value)}
-                        >
-                          {option.label}
-                        </button>
-                      ))}
-                    </div>
-                  )}
-                </div>
-
-
-                <span /* Select All Button */
-                  className="block rounded flex border-solid border-1 px-2 py-1
-                                            flex justify-center items-center text-[#3180ba] bg-[#edfdff]"
-                  style={{ width: '200px', height: '60px' }}
-                >
-                  <div className="mx-auto">
-                    <input
-                      className="mr-4 scale-150"
-                      type="checkbox"
-                      checked={checked.length === tableData.length}
-                      onChange={handleCheckAll}
-                      ref={(el) => {
-                        if (el) {
-                          el.indeterminate = checked.length > 0 && checked.length < tableData.length;
-                        }
-                      }}
-                    />
-                    Select All
+                {isDropdownOpen && (
+                  <div className="absolute right-0 mt-2 py-2 bg-white rounded shadow-lg">
+                    {categoryOptions.map((option) => (
+                      <button
+                        key={option.value}
+                        className="block px-4 py-2 text-gray-800 hover:bg-gray-100 w-full text-left"
+                        onClick={() => handleCategorySelect(option.value)}
+                      >
+                        {option.label}
+                      </button>
+                    ))}
                   </div>
-                </span>
+                )}
+              </div>
+
+              <span /* Select All Button */
+                className="block rounded flex border-solid border-1 px-2 py-1
+                                            flex justify-center items-center text-[#3180ba] bg-[#edfdff]"
+                style={{ width: '200px', height: '60px' }}
+              >
+                <div className="mx-auto">
+                  <input
+                    className="mr-4 scale-150"
+                    type="checkbox"
+                    checked={checked.length === tableData.length}
+                    onChange={handleCheckAll}
+                    ref={(el) => {
+                      if (el) {
+                        el.indeterminate = checked.length > 0 && checked.length < tableData.length;
+                      }
+                    }}
+                  />
+                  Select All
+                </div>
+              </span>
             </div>
           </div>
-        <div className="bg-white h-full overflow-y-auto rounded-b-lg drop-shadow-2xl">
-          <table className="table-auto w-full">
-            <thead>
-            <tr style={{display: isRowVisible ? 'table-row' : 'none'}}>
-                <th className="border px-4 py-2 bg-[gray] text-white"></th>
+          <div className="bg-white h-full overflow-y-auto rounded-b-lg drop-shadow-2xl z-0">
+            <table className="table-auto w-full relative">
+              <thead>
+                <tr style={{ display: isRowVisible ? 'table-row' : 'none' }}>
+                  <th className="border px-4 py-2 bg-[gray] text-white"></th>
 
-                <th className="border px-4 py-2 bg-[gray] text-white">
-                  ID
-                  <input
-                  type="text"
-                  value={searchInputs.ID}
-                  onChange={(e) => handleSearchInputChange('ID', e.target.value)}
-                  placeholder="Search ID"
-                  style={{ color: 'gray' }}
-                />
-                </th>
+                  <th className="border px-4 py-2 bg-[gray] text-white">
+                    ID
+                    <input
+                      type="text"
+                      value={searchInputs.ID}
+                      onChange={(e) => handleSearchInputChange('ID', e.target.value)}
+                      placeholder="Search ID"
+                      style={{ color: 'gray' }}
+                    />
+                  </th>
 
-                <th className="border px-4 py-2 bg-[gray] text-white">
-                  Task/Description
-                  <input
-                  type="text"
-                  value={searchInputs.Item}
-                  onChange={(e) => handleSearchInputChange('Item', e.target.value)}
-                  placeholder="Search Item"
-                  style={{ color: 'gray' }}
-                />
-                </th>
+                  <th className="border px-4 py-2 bg-[gray] text-white">
+                    Task/Description
+                    <input
+                      type="text"
+                      value={searchInputs.Item}
+                      onChange={(e) => handleSearchInputChange('Item', e.target.value)}
+                      placeholder="Search Item"
+                      style={{ color: 'gray' }}
+                    />
+                  </th>
 
-                <th className="border px-4 py-2 bg-[gray] text-white">
-                  Category
-                  <input
-                  type="text"
-                  value={searchInputs.Category}
-                  onChange={(e) => handleSearchInputChange('Category', e.target.value)}
-                  placeholder="Search Category"
-                  style={{ color: 'gray' }}
-                />
-                </th>
+                  <th className="border px-4 py-2 bg-[gray] text-white">
+                    Category
+                    <input
+                      type="text"
+                      value={searchInputs.Category}
+                      onChange={(e) => handleSearchInputChange('Category', e.target.value)}
+                      placeholder="Search Category"
+                      style={{ color: 'gray' }}
+                    />
+                  </th>
 
-                <th className="border px-4 py-2 bg-[gray] text-white">
-                  Date
-                  <input
-                  type="text"
-                  value={searchInputs.Date}
-                  onChange={(e) => handleSearchInputChange('Date', e.target.value)}
-                  placeholder="Search Date"
-                  style={{ color: 'gray' }}
-                />
-                </th>
+                  <th className="border px-4 py-2 bg-[gray] text-white">
+                    Date
+                    <input
+                      type="text"
+                      value={searchInputs.Date}
+                      onChange={(e) => handleSearchInputChange('Date', e.target.value)}
+                      placeholder="Search Date"
+                      style={{ color: 'gray' }}
+                    />
+                  </th>
 
-                <th className="border px-4 py-2 bg-[gray] text-white">
-                  Status
-                  <input
-                  type="text"
-                  value={searchInputs.Status}
-                  onChange={(e) => handleSearchInputChange('Status', e.target.value)}
-                  placeholder="Search Status"
-                  style={{ color: 'gray' }}
-                />
-                </th>
-              </tr>
-              <tr>
-                <th className="border px-4 py-2 bg-[#3180BA] text-white"></th>
-                <th className="border px-4 py-2 bg-[#3180BA] text-white">ID</th>
-                <th className="border px-4 py-2 bg-[#3180BA] text-white">Task/Description</th>
-                <th className="border px-4 py-2 bg-[#3180BA] text-white">Category</th>
-                <th className="border px-4 py-2 bg-[#3180BA] text-white">Date</th>
-                <th className="border px-4 py-2 bg-[#3180BA] text-white">Status</th>
-              </tr>
-            </thead>
-            <tbody className="">
-              {filteredTableData
-                  .map((row) => (
+                  <th className="border px-4 py-2 bg-[gray] text-white">
+                    Status
+                    <input
+                      type="text"
+                      value={searchInputs.Status}
+                      onChange={(e) => handleSearchInputChange('Status', e.target.value)}
+                      placeholder="Search Status"
+                      style={{ color: 'gray' }}
+                    />
+                  </th>
+                </tr>
+                <tr>
+                  <th className="border px-4 py-2 bg-[#3180BA] text-white"></th>
+                  <th className="border px-4 py-2 bg-[#3180BA] text-white">ID</th>
+                  <th className="border px-4 py-2 bg-[#3180BA] text-white">Task/Description</th>
+                  <th className="border px-4 py-2 bg-[#3180BA] text-white">Category</th>
+                  <th className="border px-4 py-2 bg-[#3180BA] text-white">Date</th>
+                  <th className="border px-4 py-2 bg-[#3180BA] text-white">Status</th>
+                </tr>
+              </thead>
+              <tbody className="">
+                {filteredTableData.map((row) => (
                   <tr className="hover:bg-tableHover hover:shadow-lg" key={row.ID}>
                     <td className="px-4 py-2">
                       <input
@@ -369,20 +368,20 @@ const Dashboard = () => {
                                                     hover:border-gray-400 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
                       >
                         {statusOptions.map((option) => (
-                        <option key={option.value} value={option.value}>
-                          {option.label}
-                        </option>
-                      ))}
+                          <option key={option.value} value={option.value}>
+                            {option.label}
+                          </option>
+                        ))}
                       </select>
                     </td>
                   </tr>
                 ))}
-            </tbody>
-          </table>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
 };
 
