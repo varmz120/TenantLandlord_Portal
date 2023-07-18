89447ea92263f1d2876640e32f70ed3d27f2e4f2
Darren Chan Yu Hao <darrenchanyuhao@gmail.com>
2023-07-19 03:06:32 +0800

    Updated all error page to be mobile responsive
    Updated 401, 403, 404 and runtime error page to be mobile responsive

--- src/pages/403.tsx
+++ src/pages/403.tsx
@@ -8,15 +8,15 @@ function _403_ErrorPage() {
     <React.Fragment>
       {
         <div className="bg-content flex flex-col h-screen justify-center items-center">
-          <div className="relative flex bg-form border-gray-700 rounded-lg shadow sm:p-5  h-128 w-128 justify-center items-center">
-            <form className="space-y-10">
-              <p className="text-5xl text-center font-bold h-5">403 - Forbidden</p>
-              <p className="text-l text-center font-medium h-5">
+          <div className="relative flex bg-form border-gray-700 rounded-lg shadow p-5 h-64 w-64 md:h-128 md:w-128 justify-center items-top md:items-center">
+            <form className="md:space-y-10">
+              <p className="text-3xl md:text-5xl text-center font-bold h-16 md:h-5">403 - Forbidden</p>
+              <p className="text-l text-center font-medium h-10 md:h-5">
                 You do not have permission to access this resource. If you think this is an error,
                 please contact the system admin.
               </p>
-              <div className="absolute bottom-10 right-6">
-                <ReturnToHomePage link={'/'} />
+              <div className="absolute bottom-5 right-2 md:bottom-10 md:right-6">
+                <ReturnToHomePage link={'/'} message_type={1} />
               </div>
             </form>
           </div>
