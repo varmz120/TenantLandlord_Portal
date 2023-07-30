87fdd416ff3a5a3af6f235b5bd962a074409162f
Darren Chan Yu Hao <darrenchanyuhao@gmail.com>
2023-07-31 02:15:53 +0800

    Trying to fix href anchor issue

--- src/pages/ViewAllAccounts.tsx
+++ src/pages/ViewAllAccounts.tsx
@@ -119,14 +119,14 @@ const ViewAllAccounts = () => {
       setUserType('Tenants');
     }
     setInitialRender(false);
-  }, [handleClick, handleToggle,initialRender]);
+  }, [initialRender]);
 
   return (
     <>
       <div className={`h-auto bg-[#ECEDED] flex-1 ${isClicked ? 'opacity-20' : ''}`}>
-        <a href="#">
+        <a href="/#">
           <div className="flex items-center ml-5 mt-5" onClick={handleBack}>
-            <img src={BackArrowIcon}></img>
+            <img src={BackArrowIcon} alt='back arrow'></img>
             <p className="ml-5 text-xl">Back to Panel</p>
           </div>
         </a>
