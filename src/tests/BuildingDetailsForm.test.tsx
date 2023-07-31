fd11f31c0b417297f59e863e7f86d2f332250c95
jonechong <jone.chong99@gmail.com>
2023-08-01 01:06:30 +0800

    Added BuildingDetailsForm.test.tsx

---
+++ src/tests/BuildingDetailsForm.test.tsx
@@ -0,0 +1,83 @@
+import { render, fireEvent, screen } from '@testing-library/react';
+import BuildingDetailsForm from '../components/BuildingDetailsForm';
+
+describe('<BuildingDetailsForm />', () => {
+    const handleDelClickMock = jest.fn();
+    let container: HTMLElement;
+
+    beforeEach(() => {
+      const result = render(<BuildingDetailsForm handleDelClick={handleDelClickMock} />);
+      container = result.container;
+    });
+
+    it('shows error when name is empty', async () => {
+      const nameField = container.querySelector('#Name');
+      if(!nameField) throw new Error('Cannot find element with id Name');
+
+      const submitButton = container.querySelector('button[type="submit"]');
+      if(!submitButton) throw new Error('Cannot find submit button');
+
+      fireEvent.click(submitButton);
+      expect(await screen.findByText("Name is required")).toBeInTheDocument();
+    });
+
+    it('shows error when address is empty', async () => {
+      const nameField = container.querySelector('#Name');
+      if(!nameField) throw new Error('Cannot find element with id Name');
+      const addressField = container.querySelector('#description'); 
+      if(!addressField) throw new Error('Cannot find element with id description');
+
+      const submitButton = container.querySelector('button[type="submit"]');
+      if(!submitButton) throw new Error('Cannot find submit button');
+
+      fireEvent.change(nameField, { target: { value: 'Test Name' } });
+      fireEvent.click(submitButton);
+      const errorMsg = await screen.findByText("Address is required");
+  
+      expect(errorMsg).toBeInTheDocument();
+    });
+
+    it('shows error when postal code is empty', async () => {
+      const nameField = container.querySelector('#Name');
+      if(!nameField) throw new Error('Cannot find element with id Name');
+      const addressField = container.querySelector('#description'); 
+      if(!addressField) throw new Error('Cannot find element with id description');
+      const postalCodeField = container.querySelector('[id="Postal Code"]');
+      if(!postalCodeField) throw new Error('Cannot find postal code element');
+
+      const submitButton = container.querySelector('button[type="submit"]');
+      if(!submitButton) throw new Error('Cannot find submit button');
+
+      fireEvent.change(nameField, { target: { value: 'Test Name' } });
+      
+      const changeEvent = new Event('input', { bubbles: true });
+      addressField.textContent = '123 Street';
+      addressField.dispatchEvent(changeEvent);
+
+      fireEvent.change(postalCodeField, { target: { value: '' } });
+      fireEvent.click(submitButton);
+      expect(await screen.findByText("Postal Code is required")).toBeInTheDocument();
+    });
+
+    it('shows error when postal code is not exactly 6 digits', async () => {
+      const nameField = container.querySelector('#Name');
+      if(!nameField) throw new Error('Cannot find element with id Name');
+      const addressField = container.querySelector('#description'); 
+      if(!addressField) throw new Error('Cannot find element with id description');
+      const postalCodeField = container.querySelector('[id="Postal Code"]');
+      if(!postalCodeField) throw new Error('Cannot find postal code element');
+
+      const submitButton = container.querySelector('button[type="submit"]');
+      if(!submitButton) throw new Error('Cannot find submit button');
+
+      fireEvent.change(nameField, { target: { value: 'Test Name' } });
+
+      const changeEvent = new Event('input', { bubbles: true });
+      addressField.textContent = '123 Street';
+      addressField.dispatchEvent(changeEvent);
+
+      fireEvent.change(postalCodeField, { target: { value: '123' } });
+      fireEvent.click(submitButton);
+      expect(await screen.findByText("Postal Code must be exactly 6 digits")).toBeInTheDocument();
+    });
+});
