82f5e0daa16c17d2f7ecd677e415636b57f89188
hengjiaming <hjmsoc@gmail.com>
2023-08-06 20:20:44 +0800

    8.20pm

---
+++ src/tests/LandlordAccountCreation.test.tsx
@@ -0,0 +1,48 @@
+import { render, screen, fireEvent, within } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import LandlordAccountCreation from '../pages/LandlordAccountCreation';
+
+let mockNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockNavigate,
+}));
+
+describe('LandlordAccountCreation', () => {
+
+
+  beforeEach(() => {
+    // Reset mockNavigate before each test to clean up previous interactions.
+    mockNavigate.mockReset();
+    render(
+      <Router>
+        <LandlordAccountCreation />
+      </Router>
+    );
+  });
+
+  test('renders without crashing', () => {});
+
+  test('displays "Add Service" text', () => {
+    // Assert that "Add Service" is displayed
+    expect(screen.getByText(/Add Service/i)).toBeInTheDocument();
+  });
+
+  test('displays "Service Details" text', () => {
+    // Assert that "Service Details" is displayed
+    expect(screen.getByText(/Service Details/i)).toBeInTheDocument();
+  });
+
+  test('displays "Name" text', () => {
+    // Assert that "Name" is displayed with the correct value
+    expect(screen.getByText(/Name/i)).toBeInTheDocument();
+  });
+
+
+  test('renders Submit button', () => {
+    const submitButton = screen.getByText(/Submit/i);
+
+    expect(mockNavigate).toHaveBeenCalledWith('/LandlordDashboard');
+  });
+});
